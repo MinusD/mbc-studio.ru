54fe3b4437a08f710343781aa06d5b9b6077e726
Fanlug <ytipytu1998@gmail.com>
2022-07-24 12:54:25 +0300

    1.0 Folders structures and tailwind

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="transition ease-in-out delay-150 m-5 shadow-2xl rounded-2xl h-14 bg-gradient-to-t from-sky-600 to-sky-600 hover:-translate-y-1 hover:duration-200 " >
+
+        </div>
+    );
+};
+
+export default Navbar;
